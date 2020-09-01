898827dfb2c019d5d47c9a3c12c533c6cb074ee8
HanNabu <uniondirfolder@gmail.com>
2020-09-01 20:39:46 +0300

    ++

--- SimplestPlanningSystem/UserInterface/Main.cs
+++ SimplestPlanningSystem/UserInterface/Main.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -66,12 +67,45 @@ namespace SimplestPlanningSystem
 
         private void btnLoadFromfile_Click(object sender, EventArgs e)
         {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "sps files (*.sps)|*.sps|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
 
+                if (openFileDialog.ShowDialog() == DialogResult.OK) 
+                {
+                    SPSBox box = new SPSBox();
+                    box.FilePath = openFileDialog.FileName;
+                    Dispatcher.SendServiceCode(SPSServiceCode.CreateToDoList, Dispatcher.GetController(), box);
+                    box.Dispose();
+                }
+
+            }
         }
 
         private void btnSaveToFile_Click(object sender, EventArgs e)
         {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
 
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, listView.Items.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
